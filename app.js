1c50db99d983100e1e71189543b5163c070239ac
barbaragrinop <barbarapereira123@hotmail.com>
2024-01-22 22:19:48 -0300

    adc html pages and returining them

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
+const path = require('path')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-
 app.use(bodyparser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
