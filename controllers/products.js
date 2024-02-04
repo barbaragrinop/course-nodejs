8c448d18e960edbce00f8fd88e735cd1e4bdacc2
barbaragrinop <barbarapereira123@hotmail.com>
2024-02-04 19:06:39 -0300

    finished chapter 7 - MVC

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,16 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    // this part is now in the model and made for the callback function
+    // this way the callback function is called after the products are fetched
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
 };
--- models/product.js
+++ models/product.js
@@ -1,16 +1,38 @@
 const products = []
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(require.main.filename), 'data', 'products.json'
+    //where is the file located and the name of the file
+);
+
+const getProductsFromFile = (callbackFunction) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return callbackFunction([]);
+        }
+        callbackFunction(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
-    save() {
-        products.push(this);
+    save() { //save the product to the file products.json in the data folder
+
+        getProductsFromFile((products) => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            })
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(callbackFunction) {
+        getProductsFromFile(callbackFunction);
     }
 
 }
