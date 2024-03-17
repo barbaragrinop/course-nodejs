f6a09d8d54a16e88973ef3d5ea173d8882ce3e94
barbaragrinop <barbarapereira123@hotmail.com>
2024-03-17 18:55:29 -0300

    add crud methods with sequelize

--- 11 - undestanding sequelize/controllers/admin.js
+++ 11 - undestanding sequelize/controllers/admin.js
@@ -14,12 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  Product.create({
+
+  req.user.createProduct({
     title: title,
     price: price,
     imgUrl: imageUrl,
     description: description,
-    userId: req.user.id
   }).then(result => {
     console.log('Product created!')
     res.redirect('/admin/products')
@@ -39,7 +39,7 @@ exports.postEditProduct = (req, res, next) => {
     product.imgUrl = updatedImgUrl;
     product.description = updatedDesc;
     return product.save()
-  }).then(result => {
+  }).then(() => {
     console.log('Product Updated')
     res.redirect('/admin/products')
   }).catch(err => console.log("error in posteditProduct", err))
@@ -56,15 +56,11 @@ exports.getEditProduct = (req, res, next) => {
 
   const prodId = req.params.productId;
 
-  Product.findByPk(prodId).then(product => {
-
+  req.user.getProducts({ where: { id: prodId } }).then(products => {
+    const product = products[0]
     if (!product) {
-
-      return res.redirect('/');
-
+      return res.redirect('/')
     }
-
-
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
@@ -76,7 +72,7 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
